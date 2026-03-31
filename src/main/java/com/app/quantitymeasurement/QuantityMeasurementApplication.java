0f0a5d115c81474a5ae5e40be12614e39d177c25
tiwariagam-1611 <tiwariagam16112003@gmail.com>
2026-03-31 19:28:59 +0530

    Coded QuantityMeasurementApplication to Initialize spring boot app

---
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementApplication.java
@@ -0,0 +1,12 @@
+package com.app.quantitymeasurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantityMeasurementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantityMeasurementApplication.class, args);
+    }
+}
