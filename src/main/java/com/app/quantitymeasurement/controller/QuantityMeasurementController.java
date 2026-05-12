22347ccc9075b7141348ce7019b7ef91c291bc12
tiwariagam-1611 <tiwariagam16112003@gmail.com>
2026-05-12 11:41:53 +0530

    Code Changed for AWS Deployment

--- src/main/java/com/app/quantitymeasurement/controller/QuantityMeasurementController.java
+++ src/main/java/com/app/quantitymeasurement/controller/QuantityMeasurementController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import com.app.quantitymeasurement.model.*;
 
 @RestController
-@RequestMapping("/api/v1/quantities")
+@RequestMapping("/api/quantities")
+@CrossOrigin(origins = "http://localhost:3000")
 public class QuantityMeasurementController {
 
     @Autowired
